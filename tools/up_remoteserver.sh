b706fe4f9907cca17c9818fcd53cd0379a505e56
xnlla <156630485+xnlla@users.noreply.github.com>
2023-12-03 12:01:26 +0900

    不要になったサーバーツールを移動

--- tools/up_remoteserver.sh
+++ tools/up_remoteserver.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 cd $(dirname $0)
+cd ..
+pwd
+
 if [[ ! -e .env ]];then
     echo "ERROR: create .env file."
     exit 1;
 fi
 source ./.env
-
-git branch --contains | grep "*" | grep main >/dev/null
-if [[ $? -ne 0 ]];then
-    echo "ERROR: here is no main branch."
-    exit 1;
-fi
 
 # 再帰削除/アップロードにlftpを利用する
 # オープンソースウェアだから信頼性はあるはず https://github.com/lavv17/lftp
@@ -18,14 +15,11 @@ apt list --installed 2>/dev/null | grep lftp >/dev/null
 [[ $? -ne 0 ]] && sudo apt-get install lftp -y
 lftp --version | head -1
 
+# 記事追加の場合はリソースの削除を行わず、
 lftp -u ${FTP_USER},${FTP_PASS} $FTP_URL << EOF
-rm -rf ./${FTP_DIRECTORY}
-mkdir ./${FTP_DIRECTORY}
 cd ./${FTP_DIRECTORY}
-put ./.htaccess
-put ./robots.txt 
 lcd ./docs
-mirror -R
+mirror -R --overwrite --ignore-time
 EOF
 
 echo "Complete."
