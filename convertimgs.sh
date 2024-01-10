37a06e4f2b26751c5b2a910be892f8f0bbb6d5ca
nkte8 <nkte8@outlook.jp>
2024-01-10 14:01:52 +0000

    blueskyのアカウントをリストに追加

--- convertimgs.sh
+++ convertimgs.sh
@@ -4,24 +4,24 @@ cd $(dirname $0)
 apt list --installed 2>/dev/null | grep pngquant >/dev/null
 [[ $? -ne 0 ]] && sudo apt-get install pngquant -y
 
-apt list --installed 2>/dev/null | grep convert >/dev/null
+apt list --installed 2>/dev/null | grep imagemagick >/dev/null
 [[ $? -ne 0 ]] && sudo apt install imagemagick -y
 
 GIT_UNCOMPRESSED_FILES=$(git status | grep .png | xargs)
 echo "compless... $GIT_UNCOMPRESSED_FILES"
 pngquant --ext .png --force $GIT_UNCOMPRESSED_FILES
 
-CARDS_SRC="./public/cards"
-for filepath in $GIT_UNCOMPRESSED_FILES;do 
-    echo "$filepath" | grep "images/" 
-    [[ $? -ne 0 ]] && continue;
-    FNAME=$(basename $filepath)
-    if [[ ! -e "${CARDS_SRC}/${FNAME}" ]];then
-        echo "create card of $filepath";
-            convert "$filepath" -resize 1200x1200 - \
-            | convert - -crop 1200x600+0+200 "${CARDS_SRC}/${FNAME}"
-    else
-        echo "already exists.. ${CARDS_SRC}/${FNAME}"
-    fi
-done
+# CARDS_SRC="./public/cards"
+# for filepath in $GIT_UNCOMPRESSED_FILES;do 
+#     echo "$filepath" | grep "images/" 
+#     [[ $? -ne 0 ]] && continue;
+#     FNAME=$(basename $filepath)
+#     if [[ ! -e "${CARDS_SRC}/${FNAME}" ]];then
+#         echo "create card of $filepath";
+#             convert "$filepath" -resize 1200x1200 - \
+#             | convert - -crop 1200x600+0+200 "${CARDS_SRC}/${FNAME}"
+#     else
+#         echo "already exists.. ${CARDS_SRC}/${FNAME}"
+#     fi
+# done
 echo "Complete!"
